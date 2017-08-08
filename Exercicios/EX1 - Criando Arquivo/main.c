6f06408884969c9f34ddb9881970f7b4f79bb638
macacos2 <lucascoliado@hotmail.com>
2017-08-08 14:47:37 -0300

    Criada utils.h

--- Exercicios/EX1 - Criando Arquivo/main.c
+++ Exercicios/EX1 - Criando Arquivo/main.c
@@ -1,21 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <locale.h>
-#ifdef _WIN32
-#include <conio.h>
-#endif 
-#define TAMANHO_STRING 255
-
-
-int lerInteiro(char *);
-float lerFloat(char *);
-void aplicarString(char *, char *);
-//void criarCaminho(char *, char *);
-void limparTela();
-void limparEntrada();
-void inscreverAlunos(int, FILE *);
-FILE * criarArquivo(char *);
+#include "utils.h"
 
 typedef struct Aluno
 {
@@ -25,6 +8,7 @@ typedef struct Aluno
     float nota;
 } Aluno;
 
+void inscreverAlunos(int, FILE *);
 void imprimirAlunos(Aluno *, int, FILE *);
 Aluno * alocarStruct(int);
 
@@ -85,88 +69,3 @@ void inscreverAlunos(int numeroAlunos, FILE * arquivo)
 	imprimirAlunos(alunos,numeroAlunos,arquivo);
 }
 
-/*void criarCaminho(char* entrada, char* nomePasta)
-{
-		#ifdef _WIN32
-		strcat(nomePasta,"\\");
-		#endif
-		#ifdef linux
-		strcat(nomePasta,"/");
-		#endif
-		strcat(entrada,nomePasta);
-}*/
-void limparEntrada()
-{
-	fflush(stdin);
-	getchar();
-}
-
-void limparTela()
-{
-	#ifdef _WIN32
-	system("cls");
-	#endif
-	#ifdef linux
-	system("clear");
-	#endif
-}
-
-int lerInteiro(char *mensagem)
-{
-	int numero;
-	int retorno;
-	printf("%s", mensagem);
-	retorno = scanf("%d", &numero);
-	while(retorno!=1)
-	{
-		limparTela();
-		limparEntrada();
-		printf("%s", mensagem);
-		retorno = scanf("%d", &numero);
-	}
-	limparEntrada();
-	return numero;
-}
-
-float lerFloat(char *mensagem)
-{
-	float numero;
-	int retorno;
-	printf("%s", mensagem);
-	retorno = scanf("%f", &numero);
-	while(retorno!=1)
-	{
-		limparTela();
-		limparEntrada();
-		printf("%s", mensagem);
-		retorno = scanf("%f", &numero);
-	}
-	limparEntrada();
-	return numero;
-}
-
-
-void aplicarString(char *destino, char *mensagem)
-{
-	// https://stackoverflow.com/questions/25798977/returning-string-from-c-function
-    // static char entrada[255]; impossivel reutilizar sem liberar memoria // alocação dinamica anonima
-    char entrada[TAMANHO_STRING];
-    printf("%s\n", mensagem);
-	fgets(entrada, TAMANHO_STRING-1, stdin);
-	entrada[strlen(entrada) - 1] = '\0'; //retira \n do fgets()
-	strcpy(destino, entrada);
-}
-
-FILE * criarArquivo(char *nome)
-{
-    FILE *arquivo = fopen(nome, "w");
-    if (arquivo==NULL)
-    {
-        perror("Não foi possível criar o arquivo");
-        exit(EXIT_FAILURE);
-    }
-    else
-    {
-       return arquivo;
-    }
-}
---
+++ Exercicios/EX1 - Criando Arquivo/utils.h
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+
+#define TAMANHO_STRING 255
+#ifdef _WIN32
+#include <conio.h>
+#endif 
+
+int lerInteiro(char *);
+float lerFloat(char *);
+void aplicarString(char *, char *);
+//void criarCaminho(char *, char *);
+void limparTela();
+void limparEntrada();
+FILE * criarArquivo(char *);
+
+
+/*void criarCaminho(char* entrada, char* nomePasta)
+{
+		#ifdef _WIN32
+		strcat(nomePasta,"\\");
+		#endif
+		#ifdef linux
+		strcat(nomePasta,"/");
+		#endif
+		strcat(entrada,nomePasta);
+}*/
+void limparEntrada()
+{
+	fflush(stdin);
+	getchar();
+}
+
+void limparTela()
+{
+	#ifdef _WIN32
+	system("cls");
+	#endif
+	#ifdef linux
+	system("clear");
+	#endif
+}
+
+int lerInteiro(char *mensagem)
+{
+	int numero;
+	int retorno;
+	printf("%s", mensagem);
+	retorno = scanf("%d", &numero);
+	while(retorno!=1)
+	{
+		limparTela();
+		limparEntrada();
+		printf("%s", mensagem);
+		retorno = scanf("%d", &numero);
+	}
+	limparEntrada();
+	return numero;
+}
+
+float lerFloat(char *mensagem)
+{
+	float numero;
+	int retorno;
+	printf("%s", mensagem);
+	retorno = scanf("%f", &numero);
+	while(retorno!=1)
+	{
+		limparTela();
+		limparEntrada();
+		printf("%s", mensagem);
+		retorno = scanf("%f", &numero);
+	}
+	limparEntrada();
+	return numero;
+}
+
+
+void aplicarString(char *destino, char *mensagem)
+{
+	// https://stackoverflow.com/questions/25798977/returning-string-from-c-function
+    // static char entrada[255]; impossivel reutilizar sem liberar memoria // alocação dinamica anonima
+    char entrada[TAMANHO_STRING];
+    printf("%s\n", mensagem);
+	fgets(entrada, TAMANHO_STRING-1, stdin);
+	entrada[strlen(entrada) - 1] = '\0'; //retira \n do fgets()
+	strcpy(destino, entrada);
+}
+
+FILE * criarArquivo(char *nome)
+{
+    FILE *arquivo = fopen(nome, "w");
+    if (arquivo==NULL)
+    {
+        perror("Não foi possível criar o arquivo");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+       return arquivo;
+    }
+}
