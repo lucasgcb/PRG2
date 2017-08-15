438b1a3011522d82f11a9dd62d791248144d8ac9
Lucas Bandeira <lucascoliado@hotmail.com>
2017-08-15 11:16:51 -0300

    Adicionar Exercicio 3
    Alocação dinâmica com cálculo de variação média.

---
+++ Exercicios/EX3 - Alocação Dinâmica/main.c
@@ -0,0 +1,71 @@
+#include "utils.h"
+float somarValores(float * , int );
+float somarDiferenca(float * , int , float );
+void inserirValores(float * , int );
+void imprimirValores(float * , int );
+int main()
+{
+   setlocale(LC_CTYPE, "");
+
+   float varianca = 0;
+   float media = 0;
+   int posicoes = 0;
+
+   posicoes = lerInteiro("Entre o número de posições:\n");
+   float* numeros = malloc(sizeof(float) * posicoes);
+   inserirValores(numeros, posicoes);
+   //imprimirValores(numeros, posicoes);
+
+   media = (somarValores(numeros, posicoes)) / posicoes;
+   varianca = (somarDiferenca(numeros,posicoes,media)) / posicoes;
+
+   printf("media: %f\n", media);
+   printf("varianca: %f", varianca);
+
+   return EXIT_SUCCESS;
+}
+
+void imprimirValores(float * valores, int posicoes)
+{
+   int contador = 0;
+   for(contador = 0 ; contador<posicoes ; contador++)
+   {
+      printf("Pos %d - ", contador+1);
+      printf("%f\n", valores[contador]);
+   }
+
+}
+
+void inserirValores(float * valores, int posicoes)
+{
+   int contador = 0;
+   for(contador = 0 ; contador<posicoes ; contador++)
+   {
+      printf("Pos %d - ", contador+1);
+      valores[contador] = lerFloat("Entre o número da posição:\n");
+   }
+}
+
+
+float somarDiferenca(float * valores, int posicoes, float diferenca)
+{
+   float soma = 0;
+   int contador = 0;
+   for(contador = 0 ; contador<posicoes ; contador++)
+   {
+      soma += (valores[contador] - diferenca) * (valores[contador] - diferenca);
+   }
+   return soma;
+}
+
+
+float somarValores(float * valores, int posicoes)
+{
+   float soma = 0;
+   int contador = 0;
+   for(contador = 0 ; contador<posicoes ; contador++)
+   {
+      soma += valores[contador];
+   }
+   return soma;
+}
