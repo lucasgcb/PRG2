0501d4e3c913c23cd6d4345261d725a7849b0717
Aluno <lucascoliado@hotmail.com>
2017-09-05 17:25:12 -0300

    Avancar exemplo lista encadeada

--- Exercicios/EX4 - Lista Encadeada/minhaLista/minhaLista.c
+++ Exercicios/EX4 - Lista Encadeada/minhaLista/minhaLista.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "utils.h"
 
+#define PRINCIPAL -1
+#define IMPRIMIR 1
+#define ADD 2
+#define ADDC 3
+#define REMOVER 4
+#define BUSCAR 5
+#define SAIR 0
+//#define DEBUG
 
 typedef struct no
 {
@@ -19,13 +28,16 @@ int tamanhoLista(Lista* lista);
 void imprime_lista(Lista*);
 Lista * alocarLista();
 Node * alocarNode();
+int contem(Lista*,int);
+int removerNo(Lista*, int );
 void adicionarNode(Lista*, int);
+void adicionarCabeca(Lista*, int);
+void menu(int);
+int vazia(Lista*);
 int main()
 {
     Lista *listaPtr=alocarLista();
-	Node *cabeca=alocarNode();
-	listaPtr->cabeca=cabeca;
-	listaPtr->rabo=cabeca;
+    bool sair=0;
     /*(*listaPtr).tamanho = 0;
     (*noPtr).dado = 10;
     (*noPtr).proximo = NULL;
@@ -37,13 +49,54 @@ int main()
     noPtr->proximo = NULL;
     listaPtr->cabeca = noPtr;
     listaPtr->tamanho++;*/
-    
-    adicionarNode(listaPtr, 10);
-    adicionarNode(listaPtr, 20);
-    imprime_lista(listaPtr);
+    while(!sair)
+	{
+		menu(PRINCIPAL);
+		switch(lerInteiro("\n#Digite uma opção:\n"))
+		{
+			case IMPRIMIR: 
+					limparTela();
+					imprime_lista(listaPtr);
+					esperarInput(NULL);
+					break;
+			case BUSCAR:
+					limparTela();
+					if(contem(listaPtr, lerInteiro("Entre um numero para pesquisar: ")))
+						printf("Entrada existe");
+					else
+						printf("Entrada não existe");
+					esperarInput(NULL);
+					break;
+			case ADD:
+					limparTela();
+					adicionarNode(listaPtr, lerInteiro("Entre um numero para adicionar na lista: "));
+					esperarInput(NULL);
+					break;
+			case ADDC:
+					limparTela();
+					adicionarCabeca(listaPtr, lerInteiro("Entre um numero para adicionar no topo da lista: "));
+					esperarInput(NULL);
+					break;
+			case REMOVER:	
+					limparTela();
+					if(removerNo(listaPtr, lerInteiro("Entre um numero para retirar da lista: ")))
+						printf("Numero removido\n");
+					else
+						printf("Numero não encontrado\n");
+					esperarInput(NULL);
+					break;
+			case SAIR: 
+					printf("Adeus.\n");
+					sair = true;
+					break;
+			default:
+					printf("Não implementado.\n");
+					esperarInput(NULL);
+		}
+	}
     return tamanhoLista(listaPtr);
 }
-
+	
 void imprime_lista(Lista* lista)
 {
 	Node* cursor = lista->cabeca;
@@ -70,14 +123,104 @@ Lista * alocarLista()
 	return lista;
 }
 
+int contem(Lista *lista, int pesquisa)
+{
+	Node* cursor = lista->cabeca;
+	while(cursor!=NULL)
+	{
+			if(pesquisa==cursor->dado)
+				return true;
+			cursor = cursor->proximo;
+	}
+	return false;
+}
+
+int removerNo(Lista *lista, int desejado)
+{
+	Node* aux = NULL;
+	Node* cursor = lista->cabeca;
+	if(cursor!=NULL && cursor->dado==desejado)
+	{
+		if(cursor->proximo==NULL)
+		{
+			free(cursor);
+			cursor=aux;
+			lista->cabeca=NULL;
+			lista->tamanho--;
+		}
+		else
+		{
+			aux = cursor->proximo;
+			lista->cabeca=cursor->proximo;
+			free(cursor);
+			lista->tamanho--;
+		}
+		return true;
+	}
+	while(cursor!=NULL)
+	{
+		if(cursor->proximo!=NULL)
+		{
+			if(cursor->proximo->dado==desejado)
+			{
+				if(cursor->proximo->proximo!=NULL)
+				{
+					aux=cursor->proximo->proximo;
+					free(cursor->proximo);
+					cursor->proximo=aux;
+					lista->tamanho--;
+				}
+				else
+				{
+					free(cursor->proximo);
+					cursor->proximo=aux;
+					lista->rabo=cursor;
+					lista->tamanho--;
+				}
+				return true;
+			}
+		}
+		cursor = cursor->proximo;
+	}
+	return false;
+}
+
+
 void adicionarNode(Lista* lista, int dado)
 {
 	Node *novoNode = alocarNode();
 	novoNode->dado = dado;
 	novoNode->proximo = NULL;
-	lista->rabo->proximo=novoNode;
+	if(lista->cabeca==NULL)
+		lista->cabeca=novoNode;
+	if(lista->rabo!=NULL)
+		lista->rabo->proximo=novoNode;
 	lista->rabo=novoNode;
 	lista->tamanho++;
+}
+
+
+
+
+void adicionarCabeca(Lista* lista, int dado)
+{
+	Node *novoNode = alocarNode();
+	novoNode->dado = dado;
+	if(lista->cabeca==NULL)
+		novoNode->proximo = NULL;
+	else
+		novoNode->proximo = lista->cabeca;
+	lista->cabeca=novoNode;
+	if(lista->rabo==NULL)
+			lista->rabo=novoNode;
+	lista->tamanho++;
+}
+
+int vazia(Lista* lista)
+{
+	if(lista==NULL)
+		return true;
+	return false;
 }
 
 Node * alocarNode()
@@ -90,3 +233,28 @@ Node * alocarNode()
 	p->dado = 0;
 	return p;
 }
+
+void menu(int tipo)
+{
+	char separador[] = "*************************************";
+	limparTela(); 
+    switch(tipo)
+    {
+        case PRINCIPAL:
+            printf("\n%s\n", separador);
+            printf("%d - Imprimir Lista\n", IMPRIMIR);
+            printf("%d - Adicionar No\n", ADD);
+            printf("%d - Adicionar Cabeca \n", ADDC);
+            printf("%d - Remover No \n", REMOVER);
+            printf("%d - Buscar Elemento No \n", BUSCAR);
+            printf("%d - Sair \n", SAIR);
+            printf("\n%s\n", separador);
+            break;
+        case BUSCAR:
+			printf("\n%s\n", separador);
+			printf("Buscar - ");
+			break;
+        default: printf("ue");
+    }
+}
+
---
+++ Exercicios/EX4 - Lista Encadeada/minhaLista/utils.h
@@ -0,0 +1,194 @@
+#ifndef UTILS
+#define UTILS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+#ifdef _WIN32
+#include <conio.h>
+#endif
+
+typedef unsigned char bool; //Cria tipo booleano para C
+#define true 1
+#define false 0
+#define TAMANHO_STRING 255
+
+#define DEFINIR_STRING(string) #string
+
+
+
+
+
+int lerInteiro(char *);
+int regex(char * string, char caractere1, char caractere2);
+
+float lerFloat(char *);
+
+void aplicarString(char *, char *);
+void limparTela();
+void limparEntrada();
+void esperarInput(char *);
+
+FILE * criarArquivoTexto(char *);
+FILE * abrirArquivoCSV(char *);
+
+void esperarInput(char *mensagem)
+{
+	if(mensagem==NULL)
+		printf("\nAperte ENTER para continuar...\n");
+	else
+		printf("\n%s\n", mensagem);
+		
+	getchar();
+}
+
+void limparEntrada()
+{
+	fflush(stdin);
+    #ifdef linux
+	getchar();
+	#endif
+}
+
+int lerNumeroLinhas(FILE *arquivo)
+{
+    int linhas=0;
+    char caractere;
+    while(!feof(arquivo))
+    {
+        caractere=fgetc(arquivo);
+        if(caractere=='\n')
+        {
+            linhas++;
+        }
+    }
+    rewind(arquivo);
+    return linhas;
+}
+
+
+
+void limparTela()
+{
+	#ifdef _WIN32
+	system("cls");
+	#endif
+	#ifdef linux
+	system("clear");
+	#endif
+}
+
+int lerInteiro(char *mensagem)
+{
+	int numero;
+	int retorno;
+	printf("%s", mensagem);
+	retorno = scanf("%d", &numero);
+	while(retorno!=1)
+	{
+		limparEntrada();
+		printf("%s", "Valor invalido. Tente novamente. \n");
+		printf("%s", mensagem);
+		retorno = scanf("%d", &numero);
+	}
+	limparEntrada();
+	return numero;
+}
+
+float lerFloat(char *mensagem)
+{
+	float numero;
+	int retorno;
+	printf("%s", mensagem);
+	retorno = scanf("%f", &numero);
+	while(retorno!=1)
+	{
+		limparTela();
+		limparEntrada();
+		printf("%s", mensagem);
+		retorno = scanf("%f", &numero);
+	}
+	limparEntrada();
+	return numero;
+}
+
+
+void aplicarString(char *destino, char *mensagem)
+{
+	// https://stackoverflow.com/questions/25798977/returning-string-from-c-function
+    // static char entrada[255]; impossivel reutilizar sem liberar memoria ou usando alocação dinamica anonima
+    char entrada[TAMANHO_STRING];
+    printf("%s", mensagem);
+	fgets(entrada, TAMANHO_STRING-1, stdin);
+	entrada[strlen(entrada) - 1] = '\0'; //retira \n do fgets()
+	strcpy(destino, entrada);
+}
+
+FILE * criarArquivoTexto(char *nome)
+{
+    #ifdef _WIN32
+    strcat(nome,".txt");
+    #endif // _WIN32
+    FILE *arquivo = fopen(nome, "w");
+    if (arquivo==NULL)
+    {
+        perror("Não foi possível criar o arquivo");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        return arquivo;
+    }
+}
+
+int regex(char * string, char caractere1, char caractere2) //Verifica vezes que caractere 1 é seguido por caractere 2 em uma string
+{
+	int i=0;
+	int casos=0;
+	for(i=0; i<strlen(string); i++)
+	{
+		if(string[i]==caractere1)
+		{
+			if(string[i+1]==caractere2)
+			{
+				casos++;
+			}
+			else
+				continue;
+		}
+	}
+	return casos;
+}
+
+
+int contarCaracteres(char * string, char caractere) //Verifica vezes que caractere aparece
+{
+	int i=0;
+	int casos=0;
+	for(i=0; i<strlen(string); i++)
+	{
+		if(string[i]==caractere)
+		{
+			casos++;
+		}
+	}
+	return casos;
+}
+
+FILE * abrirArquivoCSV(char *nome)
+{
+    strcat(nome,".csv");
+    FILE *arquivo = fopen(nome, "r");
+    if (arquivo==NULL)
+    {
+        perror("Não foi possível abrir o arquivo");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+       return arquivo;
+    }
+}
+
+#endif
